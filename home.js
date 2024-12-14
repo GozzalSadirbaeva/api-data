0a0cba3ed3ffec5d1ee5eb6b88d8dbac36dee5e8
GozzalSadirbaeva <sadirbaevagozzal@gmail.com>
2024-12-14 12:50:33 +0500

    updated

--- home.js
+++ home.js
@@ -1,16 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   let container = document.querySelector(".container");
+  let searchInput = document.querySelector(".searchInput");
+
   (async function () {
     let response = await fetch("https://fakestoreapi.com/users");
     let data = await response.json();
     console.log(data);
+
     data.forEach((user) => {
       let card = document.createElement("div");
       card.classList.add("card");
-      card.innerHTML = `<h2>${user.name.firstname}</h2>
+      card.innerHTML = `<h2>${user.name.firstname} ${user.name.lastname}</h2>
       <p>${user.email}</p>
-      <h2>${user.phone}</h2>`;
+      <p>${user.phone}</p>`;
       container.append(card);
+    });
+    // cards.append(card);
+    searchInput.addEventListener("input", (e) => {
+      e.preventDefault();
+      let query = e.target.value.toLowerCase();
+      let cards = document.querySelectorAll(".card");
+
+      cards.forEach((card) => {
+        let userName = card.querySelector("h2").textContent.toLowerCase();
+        if (userName.includes(query)) {
+          card.style.display = "";
+        } else {
+          card.style.display = "none";
+        }
+      });
     });
   })();
 });
